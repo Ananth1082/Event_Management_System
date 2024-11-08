ed2b63f1e49f692fd3254d0c188185398b8a0c35
Ananth1082 <ananthshetty203@gmail.com>
2024-11-08 19:37:22 +0530

    feat: setup

---
+++ src/main/java/com/jsf_mini_project/event_management_system/dto/VenueDto.java
@@ -0,0 +1,11 @@
+package com.jsf_mini_project.event_management_system.dto;
+
+import lombok.Data;
+
+@Data
+public class VenueDto {
+    private String name;
+    private String description;
+    private String address;
+    private int capacity;
+}
